0e228afeb0aa8ee90e0724848ed29f5ec95ac6a2
SILVA-NNS <nimnasilva29@gmail.com>
2026-04-08 21:13:21 +0530

    refactor: remove missing person module
    Removed all components, pages, context, hooks, and services related to the missing person feature. Updated AppRoutes to reflect these changes and clean up unused references.

--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -4,7 +4,6 @@ import DashboardLayout from "../layouts/DashboardLayout";
 import CitizenDashboard from "../pages/citizen/CitizenDashboard";
 import ShelterPage from "../pages/citizen/ShelterPage";
 import DisasterPage from "../pages/citizen/DisasterPage";
-import MissingPersonPage from "../pages/citizen/MissingPersonPage";
 
 const AppRoutes = () => {
   return (
@@ -13,7 +12,6 @@ const AppRoutes = () => {
         <Route path="/citizen-dashboard" element={<CitizenDashboard />} />
         <Route path="/citizen/shelters" element={<ShelterPage />} />
         <Route path="/citizen/disaster" element={<DisasterPage />} />
-        <Route path="/citizen/missing-persons" element={<MissingPersonPage />} />
       </Routes>
     </DashboardLayout>
   );
