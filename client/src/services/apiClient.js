1150f5060507e6c37dcee3847d16a13ebdfbb0c6
Heshan Jayasekara <heshanjayasekara18@gmail.com>
2026-04-12 04:15:54 +0530

    fix: update API URL handling and add new dependencies

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "",
+  baseURL: process.env.REACT_APP_API_URL || "",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
